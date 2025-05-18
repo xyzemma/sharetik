aa43f0978456c3c36190a4e8a1674627b0fa7a76
Emma <emmadomeisen@gmail.com>
2025-05-18 20:08:16 +0200

    Removed unnecessary comments

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,11 @@
 var serverurl = "http://127.0.0.1:5000/sharetik"
 function copyfunc() {
-    // Get the text field
     var copyText = document.getElementById("outputfield");
   
-    // Select the text field
     copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
+    copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
   
-    // Alert the copied text
     alert("Copied the text: " + copyText.value);
   } 
 function getoutput(){
@@ -18,7 +13,7 @@ function getoutput(){
     var outputfield = document.getElementById("outputfield")
     url = `${serverurl}?url=${infield.value}`
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.open( "GET", url, false );
     xmlHttp.send( null );
     var rjson = JSON.parse(xmlHttp.responseText);
     outputfield.value = rjson["message"];
